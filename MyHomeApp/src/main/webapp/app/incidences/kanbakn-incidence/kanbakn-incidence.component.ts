d0b2cdf2904e43628df21a69adc60d0fe3ec3b00
alber <alber@192.168.1.176>
2023-05-04 23:00:36 +0200

    Se empieza la segunda parte del kanban
    Falta la parte del servidor

--- MyHomeApp/src/main/webapp/app/incidences/kanbakn-incidence/kanbakn-incidence.component.ts
+++ MyHomeApp/src/main/webapp/app/incidences/kanbakn-incidence/kanbakn-incidence.component.ts
@@ -2,12 +2,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Incidence } from '../../core/model/incidence.model';
 import { IncidencesService } from '../incidences.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { IncidenceKanbanDTO } from 'app/core/model/incidenceKanbanDTO.model';
 import { Alert } from 'app/core/util/alert.service';
 import { TranslateService } from '@ngx-translate/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UpdateEmployeeIncidenceComponent } from '../update-employee-incidence/update-employee-incidence.component';
+import { DragulaService } from 'ng2-dragula';
 
 @Component({
   selector: 'jhi-kanbakn-incidence',
@@ -27,29 +28,48 @@ export class KanbaknIncidenceComponent implements OnInit, OnDestroy {
   private alertError: Alert;
 
   private findAllSubcription: Subscription | undefined;
+  //private dragulaSubcription: Subscription;
+  //private dragSubcription: Subscription;
+  private dropModelSubcription: Subscription;
 
   constructor( 
     private incidencesService: IncidencesService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private modalService: NgbModal,
-    private translateService: TranslateService) 
+    private translateService: TranslateService,
+    private dragulaService: DragulaService) 
   {
     this.alertError = { 
       type: 'danger',
       message: this.translateService.instant('incidences.form.errors.create'),
     };
+    
+    this.dropModelSubcription = this.dragulaService.dropModel("INCIDENCES_CONTAINER").subscribe(
+      ({ name: string, el: Element, target, source, sibling, item, sourceModel, targetModel, sourceIndex, targetIndex }) => {
+      console.log('dropModel event!!!');
+      if( !item || !item.id  ){
+        throw new Error('Incidence is wrong!.');
+      }
+      //Get element
+      let incidence = targetModel[targetIndex];
+      //Update element
+
+    });
+
   }
- 
   ngOnDestroy(): void {
+    //throw new Error('Method not implemented.');
     if(this.findAllSubcription !== undefined ){
       this.findAllSubcription.unsubscribe();
+    }
+    if( this.dropModelSubcription !== undefined ){
+      this.dropModelSubcription.unsubscribe();
     }
   }
 
   ngOnInit(): void {
     console.log('KanbaknIncidenceComponent');
-    this.init();
     this.initIncidences();
   }
 
@@ -94,70 +114,65 @@ export class KanbaknIncidenceComponent implements OnInit, OnDestroy {
 
     for(let i=0; i<incidences.length;i++){
       let incidence = incidences[i];
-      switch(incidence.status){
-        case 'PENDING':
-          this.incidencesPending.push(incidence);
+      this.processIncidence(incidence);
+    }
+    this.initDragAndDrop();
+  }
+
+  private processIncidence(incidence: IncidenceKanbanDTO) {
+    switch (incidence.status) {
+      case 'PENDING':
+        this.incidencesPending.push(incidence);
         break;
-        case 'IN_PROCESS':
-          this.incidencesInProgress.push(incidence);
+      case 'IN_PROCESS':
+        this.incidencesInProgress.push(incidence);
         break;
-        case 'PENDING_CONFIRM':
-          this.incidencesPendingConfirm.push(incidence);
+      case 'PENDING_CONFIRM':
+        this.incidencesPendingConfirm.push(incidence);
         break;
-        case 'RESOLVED':
-          this.incidencesResolved.push(incidence);  
+      case 'RESOLVED':
+        this.incidencesResolved.push(incidence);
         break;
-        default:
-          break;
-      }
+      default:
+        break;
     }
   }
 
-  private init(){
-    /*let incidence1 = new Incidence();
+  private initDragAndDrop(): void{
+    
+    this.dragulaService.createGroup('INCIDENCES_CONTAINER', {
+      accepts: (el?: Element | undefined, target?: Element | undefined, source?: Element | undefined, sibling?: Element | undefined)=> {
+        console.debug('accepts');
+        return this.accept(el, target, source, sibling);
+      },
+      /*copyItem: (item: any) => {
+        return item;
+      },*/
+      
+    });
 
-    incidence1.id = 1;
-    incidence1.title = 'incidencia 1';
-    incidence1.description = 'Descripcion de la incidencia 1. Esto es un texto largo que vamos a cortar porque queremos ver hasta donde llega el parrafo html';
-    incidence1.startDate = new Date();
-    //incidence1.photos = [];
-    incidence1.status = 'PENDING';
-    incidence1.priority = 'HIGH';
+    /*this.dragulaSubcription.add(this.dragulaService.drag("INCIDENCES_CONTAINER").subscribe(({ name, el, source }) => {
+      console.log('drag event!!!');
+    }));
 
-    let incidence2 = new Incidence();
 
-    incidence2.id = 2;
-    incidence2.title = 'incidencia 2';
-    incidence2.description = 'Descripcion de la incidencia 2.';
-    incidence2.startDate = new Date();
-    //incidence2.photos = [];
-    incidence2.status = 'IN_PROCESS';
-    incidence2.priority = 'MEDIUM';
 
-    let incidence3 = new Incidence();
-
-    incidence3.id = 3;
-    incidence3.title = 'incidencia 3';
-    incidence3.description = 'Descripcion de la incidencia 3.';
-    incidence3.startDate = new Date();
-    //incidence3.photos = [];
-    incidence3.status = 'PENDING_CONFIRM';
-    incidence3.priority = 'SLOW';
-
-    let incidence4 = new Incidence();
-
-    incidence4.id = 4;
-    incidence4.title = 'incidencia 4';
-    incidence4.description = 'Descripcion de la incidencia 4.';
-    incidence4.startDate = new Date();
-    //incidence4.photos = [];
-    incidence4.status = 'RESOLVED';
-    incidence4.priority = 'SLOW';
-
-    this.incidencesPending = [incidence1, incidence1, incidence1, incidence1];
-    */
+    this.dragulaSubcription.add(this.dragulaService.drop("INCIDENCES_CONTAINER").subscribe(({ name, el, target, source, sibling }) => {
+        console.log('drop event!!!');
+      })
+    );
+    // some events have lots of properties, just pick the ones you need
+    this.dragulaSubcription.add(this.dragulaService.dropModel("INCIDENCES_CONTAINER").subscribe(({ sourceModel, targetModel, item }) => {
+      console.log('dropModel event!!!');
+    }));*/
+    
   }
 
+  private accept(el?: Element, target?: Element, source?: Element, sibling?: Element): boolean{
+    return target?.id !== 'source' ;
+  }
+
+  
 
 
 }
--- MyHomeApp/src/main/webapp/app/shared/shared-libs.module.ts
+++ MyHomeApp/src/main/webapp/app/shared/shared-libs.module.ts
@@ -7,8 +7,20 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import { GoogleMapsModule } from '@angular/google-maps'; 
+import { DragulaModule } from 'ng2-dragula';
 
 @NgModule({
-  exports: [FormsModule, CommonModule, NgbModule, InfiniteScrollModule, FontAwesomeModule, ReactiveFormsModule, TranslateModule, NgxDropzoneModule, GoogleMapsModule],
+  exports: [
+    FormsModule,
+    CommonModule,
+    NgbModule,
+    InfiniteScrollModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    TranslateModule,
+    NgxDropzoneModule,
+    GoogleMapsModule,
+    DragulaModule
+  ],
 })
 export class SharedLibsModule {}
